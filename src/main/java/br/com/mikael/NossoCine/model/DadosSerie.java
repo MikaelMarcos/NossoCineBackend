5082c21eb3333a41f43c74f02e931db25dc87221
Mikael <mikael.1010.mm@gmail.com>
2024-06-28 21:10:10 -0300

    Adição de menu, com spring em java

--- src/main/java/br/com/mikael/NossoCine/model/DadosSerie.java
+++ src/main/java/br/com/mikael/NossoCine/model/DadosSerie.java
@@ -1,11 +1,10 @@
 package br.com.mikael.NossoCine.model;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-
-public record DadosSerie(@JsonAlias("Title")String titulo,
-                         @JsonAlias("totalSeasons")Integer totalTemporadas,
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
                          @JsonAlias("imdbRating") String avaliacao) {
-
 }
